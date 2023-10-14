f837266fa604a88ea44d0e0923d77ee9535889ce
Rami Leghrib <95674555+RamiLGH8@users.noreply.github.com>
2023-10-14 11:32:43 +0200

    projects!

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import '../styles/project.css'
+
 function Project (props) {
   return (
     <div className='Project-Card'>
+       <div className='informations'>
+        <div className='title'>
          <h2>{props.name}</h2>
-         <p>{props.description}</p>
-          <Button  className='project-button' variant="warning"><a href={props.link}>View Project</a></Button>
+         <i className='project-icon' class={props.icon} style={{fontSize:'40px'}}></i>
+        </div>
+        <div className='description'>
+         <p className='description-text'>{props.description}</p>
+        </div>                
+       </div>
+       <div className='button'>
+         <Button  className='project-button' variant="warning"><a className='link' href={props.link}>View Project</a></Button>
+       </div>
+          
     </div>
   );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,8 +4,9 @@ import Project from './Project'
 export const Projects = () => {
   return (
     <div id='projects'>
-        <Project name='Chat App' description='gagh' link='https://github.com/RamiLGH8/CHAT-APP' />
-        
+        <Project name='Chat App' description='Ivhfbhbjkrbjhfhjbhbsjbsfbhjfbhdhbsvhbhbdhbvshbhjsyghjbsvhjbjhbsvvsbjhdvhhduvieufibdvhbvd' link='https://github.com/RamiLGH8/CHAT-APP' icon='fa-solid fa-message' />
+        <Project name='To Do List' description='uehkjhkjvhrjbrhkkjrnvjnrjhhbrijiejmjfbfhfjjievrnbrnjijinvfbbn n fbfhbfvnnvjnvf' link='https://github.com/RamiLGH8/TO-DO-LIST' icon='fa-solid fa-list' />
+        <Project name='Calculator' description='vhgvyvegyghwvhhhdbvhfbhghfgjgrgygrvyghrvuhjhvfhjhjvrhhvbhf' link='https://github.com/RamiLGH8/Calculator' icon='fa-solid fa-calculator' />
     </div>
   )
 }
